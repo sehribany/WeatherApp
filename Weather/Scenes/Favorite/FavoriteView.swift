36760c72279dde3bf9ecb3ece56ccdd63104b155
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-05-03 18:38:19 +0300

    Scenes Created.

---
+++ Weather/Scenes/Favorite/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  Weather
+//
+//  Created by Şehriban Yıldırım on 3.05.2024.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
---
+++ Weather/Scenes/MapIntegration/MapIntegrationView.swift
@@ -0,0 +1,18 @@
+//
+//  MapIntegrationView.swift
+//  Weather
+//
+//  Created by Şehriban Yıldırım on 3.05.2024.
+//
+
+import SwiftUI
+
+struct MapIntegrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapIntegrationView()
+}
