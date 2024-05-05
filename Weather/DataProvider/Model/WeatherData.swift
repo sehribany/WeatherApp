f34bb3ea2a1d9e29164b7ee9eb31e0776955c6e6
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-05-05 17:54:48 +0300

    WeatherData Added.

---
+++ Weather/DataProvider/Model/WeatherData.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Şehriban Yıldırım on 5.05.2024.
+//
+
+import Foundation
+import CoreLocation
+
+struct WeatherData {
+    let locationName: String
+    let temperature: Double
+    let condition: String
+    let highTemp: Double
+    let lowTemp: Double
+    var coordinate: CLLocationCoordinate2D
+}
