6a25b29b7fa924c274b9490632d052b7775fd5b0
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-05-05 17:53:55 +0300

    LocationManager Created.

---
+++ Weather/Base/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Şehriban Yıldırım on 3.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Weather/DataProvider/Manager/LocationManager.swift
@@ -0,0 +1,33 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by Şehriban Yıldırım on 3.05.2024.
+//
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate{
+    private let locationManager = CLLocationManager()
+    @Published var location:CLLocation?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestLocation(){
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        self.location = location
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+        print(error.localizedDescription)
+    }
+}
